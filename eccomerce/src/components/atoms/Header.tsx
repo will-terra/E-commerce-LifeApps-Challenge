70bc7fe235c281ff4031d126060cbc851e3e2f80
will.terra <wpterra@outlook.com>
2025-01-25 13:24:01 -0300

    Feat: Componente Header

---
+++ eccomerce/src/components/atoms/Header.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <header>
+      <p className="py-4 w-full bg-black flex justify-center text-white">
+        Promoção: Frete grátis para todo Brasil acima de 99 reais{" "}
+      </p>
+    </header>
+  );
+};
+
+export default Header;
