4230ff7eebcf5ffb7c8783d766a537283819f350
will.terra <wpterra@outlook.com>
2025-01-25 17:57:26 -0300

    Feat: Fetch API

--- eccomerce/next.config.ts
+++ eccomerce/next.config.ts
@@ -4,4 +4,14 @@ const nextConfig: NextConfig = {
   /* config options here */
 };
 
+module.exports = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "s3-sa-east-1.amazonaws.com",
+      },
+    ],
+  },
+};
 export default nextConfig;
--- eccomerce/src/contexts/AppProvider.tsx
+++ eccomerce/src/contexts/AppProvider.tsx
@@ -1,14 +1,33 @@
 "use client";
-import React, { createContext, useContext, useState, ReactNode } from "react";
+import React, {
+  createContext,
+  useContext,
+  useState,
+  useEffect,
+  ReactNode,
+} from "react";
+
+interface Product {
+  id: number;
+  name: string;
+  category: string;
+  price: number;
+  discount_percentage: number;
+  promotional_price: number;
+  image: string;
+  description: string;
+}
 
 interface AppContextProps {
   selectedValue: string;
   setSelectedValue: (value: string) => void;
+  data: Product[];
 }
 
 const AppContext = createContext<AppContextProps>({
   selectedValue: "Todos",
   setSelectedValue: () => {},
+  data: [],
 } as AppContextProps);
 
 export const useAppContext = (): AppContextProps => {
@@ -27,9 +46,21 @@ export const AppContextProvider: React.FC<AppProviderProps> = ({
   children,
 }) => {
   const [selectedValue, setSelectedValue] = useState<string>("Todos");
+  const [data, setData] = useState<Product[]>([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await fetch(
+        "https://api-prova-frontend.solucoeslifeapps.com.br/products"
+      );
+      const result = await response.json();
+      setData(Array.isArray(result) ? result : [result]);
+    };
+    fetchData();
+  }, []);
 
   return (
-    <AppContext.Provider value={{ selectedValue, setSelectedValue }}>
+    <AppContext.Provider value={{ selectedValue, setSelectedValue, data }}>
       {children}
     </AppContext.Provider>
   );
