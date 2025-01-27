31a56149fb926cea2179261e9d1d8800f17c619d
will.terra <wpterra@outlook.com>
2025-01-27 17:49:43 -0300

    Style: responsividade ProductGrid

--- eccomerce/src/components/organisms/ProductGrid.tsx
+++ eccomerce/src/components/organisms/ProductGrid.tsx
@@ -4,7 +4,7 @@ import ProductCard from "../molecules/ProductCard";
 const ProductGrid = () => {
   const { filteredProducts } = useAppContext();
   return (
-    <section className="grid grid-cols-3  w-fit gap-12 place-content-center mx-auto my-4">
+    <section className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4  w-fit gap-12 place-content-center mx-auto my-4">
       {filteredProducts.map((item) => (
         <ProductCard key={item.id} {...item} />
       ))}
