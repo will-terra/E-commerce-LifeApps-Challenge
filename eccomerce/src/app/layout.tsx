312457c6f63102edebe2e82884d64573548b0151
will.terra <wpterra@outlook.com>
2025-01-30 14:43:58 -0300

    Feat: Desempenho ProductDetails Page

--- eccomerce/src/app/layout.tsx
+++ eccomerce/src/app/layout.tsx
@@ -1,9 +1,11 @@
 import type { Metadata } from "next";
+import Head from "next/head";
 import "./globals.css";
-import { AppContextProvider } from "@/contexts/AppProvider";
+
 import Header from "@/components/atoms/Header";
 import NavBar from "@/components/organisms/NavBar";
 import Footer from "@/components/molecules/Footer";
+import { AppContextProvider } from "@/contexts/AppProvider";
 
 export const metadata: Metadata = {
   title: "NEISH",
@@ -17,6 +19,12 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="pt-BR" className="global-html">
+      <Head>
+        <link
+          rel="preconnect"
+          href="https://api-prova-frontend.solucoeslifeapps.com.br"
+        />
+      </Head>
       <AppContextProvider>
         <body className="h-screen">
           <div className="flex flex-col justify-between h-full bg-gray-100 ">
