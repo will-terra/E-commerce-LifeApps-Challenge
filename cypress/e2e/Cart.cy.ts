21dcd6a88c606a9c148a231b4109661a44b28c45
will.terra <wpterra@outlook.com>
2025-01-31 12:47:15 -0300

    Feat: Dialog de pedido concluido

--- cypress/e2e/Cart.cy.ts
+++ cypress/e2e/Cart.cy.ts
@@ -15,6 +15,24 @@ describe("Cart Page", () => {
     );
     cy.get(":nth-child(2) > .flex > div > .mx-2").click();
     cy.url().should("be.equal", `${Cypress.config().baseUrl}/cart`);
+
+    cy.get('.hidden > .justify-around > [type="button"] > .px-8').click();
+    cy.get("#base-ui-\\:r7\\: > .text-lg").contains("Você concluiu o pedido!");
+    cy.get("#base-ui-\\:r7\\: > :nth-child(3) > button > .undefined").click();
+    cy.get(".lg\\:flex-row > .w-full").should("not.have.descendants");
+  });
+
+  it("Add and remove items from cart", () => {
+    cy.contains("Verão 2025");
+    cy.get(":nth-child(1) > .flex-col > .gap-2 > .bg-white").click().click();
+    cy.get(":nth-child(2) > .flex-col > .gap-2 > .bg-white").click().click();
+    cy.get(":nth-child(3) > .flex-col > .gap-2 > .bg-white").click().click();
+    cy.get(":nth-child(4) > .flex-col > .gap-2 > .bg-white").click().click();
+    cy.get(".justify-around > :nth-child(2) > .flex > .text-black").contains(
+      "8"
+    );
+    cy.get(":nth-child(2) > .flex > div > .mx-2").click();
+    cy.url().should("be.equal", `${Cypress.config().baseUrl}/cart`);
     cy.get(":nth-child(1) > .flex-col > .w-16 > .text-3xl").click();
     cy.get(".justify-around > :nth-child(2) > .flex > .text-black").contains(
       "7"
--- src/components/atoms/AssistButton.tsx
+++ src/components/atoms/AssistButton.tsx
@@ -3,6 +3,7 @@ interface AssistButtonProps {
   ariaLabel: string;
   size?: "small" | "large";
   variant?: "black" | "white";
+  className?: string;
 }
 
 const AssistButton: React.FC<AssistButtonProps> = ({
@@ -10,8 +11,9 @@ const AssistButton: React.FC<AssistButtonProps> = ({
   ariaLabel,
   size = "small",
   variant = "black",
+  className,
 }) => {
-  const options = `flex justify-center items-center cursor-pointer hover:bg-gray-600 ${
+  const options = `${className} flex justify-center items-center cursor-pointer hover:bg-gray-600 ${
     size === "small"
       ? "text-sm py-1 px-4 w-fit max-w-[7rem] "
       : "text-lg py-2  min-w-[8rem]"
