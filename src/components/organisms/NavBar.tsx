7d9e7b52de52907f0b1e6ae0e09cc97320fe3289
will.terra <wpterra@outlook.com>
2025-01-31 14:20:50 -0300

    Refactor: semântica html NavBar

--- src/components/organisms/NavBar.tsx
+++ src/components/organisms/NavBar.tsx
@@ -4,15 +4,19 @@ import ProfileIcon from "../atoms/ProfileIcon";
 
 const NavBar: React.FC = () => {
   return (
-    <nav className="flex justify-between items-center w-full py-1 bg-gray-200 border border-gray-500">
-      <Logo variant="black" />
-      <ul className="flex justify-around items-center mr-4 xl:mr-20">
+    <nav className="flex bg-gray-200 border border-gray-500">
+      <ul className="flex justify-between items-center w-full py-1">
         <li>
-          <ProfileIcon />
+          <Logo variant="black" />
         </li>
-        <li>
-          <CartActions />
-        </li>
+        <div className="flex justify-around items-center mr-4 xl:mr-20">
+          <li>
+            <ProfileIcon />
+          </li>
+          <li>
+            <CartActions />
+          </li>
+        </div>
       </ul>
     </nav>
   );
