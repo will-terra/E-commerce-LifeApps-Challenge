a344fd927f2627fa155bd4b096531ed3f04f7dd8
will.terra <wpterra@outlook.com>
2025-01-31 17:16:57 -0300

    Style: HomePage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,8 +5,8 @@ import ProductGrid from "@/components/organisms/ProductGrid";
 
 export default function Home({}) {
   return (
-    <main className="py-8 bg-gray-100">
-      <h1 className="place-self-start mx-8 text-3xl text-center font-bold">
+    <main className="flex flex-col py-8 bg-gray-100">
+      <h1 className="flex justify-center mx-8 text-3xl text-center font-bold">
         Veja nossa nova coleção Verão 2025:
       </h1>
       <Filter />
