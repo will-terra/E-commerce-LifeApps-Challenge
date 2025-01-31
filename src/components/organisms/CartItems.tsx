fdd86b663d57f1f24a6eb1efea1889b0200e9848
will.terra <wpterra@outlook.com>
2025-01-31 17:24:38 -0300

    Feat: header em CartItem

--- src/components/organisms/CartItems.tsx
+++ src/components/organisms/CartItems.tsx
@@ -7,7 +7,10 @@ const CartItems: React.FC = () => {
   const { cart } = useAppContext();
 
   return (
-    <div className="flex flex-col w-full gap-4 py-4 px-4 lg:px-0 mt-8 lg:mt-0">
+    <div className="flex flex-col w-full gap-4  px-4 lg:px-0 ">
+      <h1 className="flex text-3xl text-center font-bold ml-2 mb-1 lg:ml-24">
+        Seu carrinho:
+      </h1>
       {cart.map((item) => (
         <CartCard key={item.id} product={item} />
       ))}{" "}
